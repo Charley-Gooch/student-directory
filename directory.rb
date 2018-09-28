59e811c07526179c1700f52073959a4052ecb82c
Charley Gooch <chazgooch@gmail.com>
2018-09-28 14:43:06 +0000

    hash added

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 #array holding students
-$students = [ "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates" ]
+$students = [ { name: "Dr. Hannibal Lecter", cohort: "november" },
+ { name: "Darth Vader", cohort: "november" },
+ { name: "Nurse Ratched", cohort: "november" },
+ { name: "Michael Corleone", cohort: "november" },
+ { name: "Alex DeLarge", cohort: "november" },
+ { name: "The Wicked Witch of the West", cohort: "november" },
+ { name: "Terminator", cohort: "november" },
+ { name: "Freddy Krueger", cohort: "november" },
+ { name: "The Joker", cohort: "november" },
+ { name: "Joffrey Baratheon", cohort: "november" },
+ { name: "Norman Bates", cohort: "november" }]
 
 def header 
   puts "The students of Villains Academy"
@@ -17,8 +17,8 @@ def header
 end
 #array of students printed
 def body
-  $students.each do |x|
-    puts x
+  $students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 #finish caption
