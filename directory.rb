a5c9c6d90b2bc58f2c856a726f232ccfc4515d37
Charley Gooch <chazgooch@gmail.com>
2018-09-28 14:29:39 +0000

    refreactor to directory ruby

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 puts "The students of Villains Academy"
 puts "--------------------------------"
 #list
@@ -24,6 +25,4 @@ puts "--"
 puts "Norman Bates"
 puts "--"
 #finish caption
-print "Overall, we have "
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
